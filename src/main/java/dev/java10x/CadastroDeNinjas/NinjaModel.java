23c350d27647238007a6d4aaa357754beea9d174
Alex Sousa <alexsandro091092@gmail.com>
2026-05-15 17:11:55 -0300

    third commit - Creating an entity

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,58 @@
+package dev.java10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String aldeia;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, String aldeia, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.aldeia = aldeia;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAldeia() {
+        return aldeia;
+    }
+
+    public void setAldeia(String aldeia) {
+        this.aldeia = aldeia;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}

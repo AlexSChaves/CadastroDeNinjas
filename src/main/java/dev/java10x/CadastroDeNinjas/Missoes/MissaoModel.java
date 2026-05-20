7ed31be49a4b08f3c168b2892e804992b3f84b3b
Alex Sousa <alexsandro091092@gmail.com>
2026-05-20 10:31:49 -0300

    feat: configura variaveis de ambiente e esconde .env

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -17,8 +17,13 @@ public class MissaoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "dificuldade")
     private String dificuldade;
 
     //OneToMany, uma missão pode ter vários ninjas

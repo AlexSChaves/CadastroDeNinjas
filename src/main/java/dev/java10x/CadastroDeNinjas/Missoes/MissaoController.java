0c11bf0c43e5fe4cfcd297fbac2440d3a1b7f928
Alex Sousa <alexsandro091092@gmail.com>
2026-05-21 11:28:36 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,16 +1,40 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class MissaoController {
 
-    @GetMapping("/missoes")
-    public String boasMissoes(){
-        return "Essa é a minha primeira mensagem nessa rota de missoes";
+    //Adicionar Missões (CREATE)
+    @PostMapping("/criarMissao")
+    public String criarMissao(){
+        return "Missao criada";
     }
+
+    //Mostrar Missao por ID (READ)
+    @GetMapping("/missaoID")
+    public String mostrarMissaoPorID(){
+        return "Missão apresentado por ID";
+    }
+
+    //Mostrar todos as Missões (READ)
+    @GetMapping("/todasMissoes")
+    public String mostrarMissoes(){
+        return "Missões apresentadas";
+    }
+
+    //Alterar dados das Missões (UPDATE)
+    @PutMapping("/alterarMissaoID")
+    public String alterarMissaoPorID(){
+        return "Missão alterado por ID";
+    }
+
+    //Deletar Missão (DELETE)
+    @DeleteMapping("/deletarMissaoID")
+    public String deletarMissaoPorID(){
+        return "Missão deletada por ID";
+    }
+
 
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,16 +1,39 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
 
-        @GetMapping("/boasvindas")
-        public String boasVindas(){
-            return "Essa é a minha primeira mensagem nessa rota";
-        }
+    //Adicionar Ninjas (CREATE)
+    @PostMapping("/criarNinja")
+    public String criarNinja(){
+        return "Ninja criado";
+    }
+
+    //Mostrar Ninja por ID (READ)
+    @GetMapping("/ninjaID")
+    public String mostrarNinjaPorID(){
+        return "Ninja apresentado por ID";
+    }
+
+    //Mostrar todos os Ninjas (READ)
+    @GetMapping("/todosNinjas")
+    public String mostrarNinjas(){
+        return "Ninjas apresentados";
+    }
+
+    //Alterar dados dos Ninjas (UPDATE)
+    @PutMapping("/alterarNinjaID")
+    public String alterarNinjaPorID(){
+        return "Ninja alterado por ID";
+    }
+
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarNinjaID")
+    public String deletarNinjaPorID(){
+        return "Ninja deletador por ID";
+    }
 
 }
